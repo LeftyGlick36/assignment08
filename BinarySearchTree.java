222ff9ca461ed50f8e81d6f4053313874eac4c7e
Moses <mopedigg36@gmail.com>
2016-10-20 15:40:51 -0600

    Assignment 8: Binary Search Trees initial commit
    The Problem
    
    We have been asked to construct a program that spell-checks a document
    given a "dictionary" of valid words.
    
    We know that the key to an efficient spell-checker is an efficient
    mechanism for storing and searching the dictionary. We decide to
    maintain the dictionary (simply a list of strings) in sorted order, so
    that searching the list will be efficient. Furthermore, we decide to
    store the dictionary in a binary search tree (BST) because all
    operations on the BST are potentially very fast.
    
    We must first implement a BST of generically-typed items. The
    implementation should include a class to represent a binary tree node
    and a class to represent a BST (a collection of binary tree nodes).
    Please note that there is no requirement that the BST be balanced.
    
    We must then apply the BST to the problem of spell-checking a document,
    using a BST to store a dictionary of strings and looking up each
    document string in the BST. The implementation should include a class
    to represent a spell-checker and a class to demonstrate the
    spell-checker for various dictionary and document files.

---
+++ BinarySearchTree.java
@@ -0,0 +1,81 @@
+package assignment08;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+public class BinarySearchTree<T extends Comparable<? super T>> implements SortedSet<T> {
+
+	@Override
+	public boolean add(T item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> items) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(T item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<? extends T> items) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T first() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T last() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(T item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<? extends T> items) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<T> toArrayList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
